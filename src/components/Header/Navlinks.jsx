9d6af85daca529ae7ab368e44cc467c4d79fa9d6
asqarovilhomjon <asqarovilhomjon045@gmail.com>
2024-12-09 18:37:41 +0500

    cinema

--- src/components/Header/Navlinks.jsx
+++ src/components/Header/Navlinks.jsx
@@ -3,44 +3,42 @@ import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom'
 import { BiMovie } from 'react-icons/bi'
 import { MdOutlineTheaterComedy } from 'react-icons/md'
+import { TbTicket } from 'react-icons/tb'
 import { FiSearch } from 'react-icons/fi'
-import { BsBookmark } from "react-icons/bs";
-import { useTranslation } from 'react-i18next';
 
 const Navlinks = () => {
-    const {t, i18n} =  useTranslation()
     const Mode = useSelector((state) =>  state.isDarkMode.isDarkMode);
     const navLinks = [
         {
             id: 1,
-            name: t("header.poster"),
+            name: 'Афиша',
             icon: <BiMovie className="w-6 h-6"/>,
             link: "/"
         },
         {
             id: 2,
-            name: t("header.genre"),
+            name: 'Сеансы',
             icon: <MdOutlineTheaterComedy className="w-6 h-6"/>,
             link: "/sessions"
         },
         {
             id: 3,
-            name: t("header.favourite"),
-            icon: <BsBookmark className="w-6 h-6 p-[1px]"/>,
-            link: "/favourite"
+            name: 'Билеты',
+            icon: <TbTicket className="w-6 h-6"/>,
+            link: "/ticket"
         },
         {
             id: 4,
-            name: t("header.search"),
+            name: 'Поиск',
             icon: <FiSearch className="w-6 h-6"/>,
             link: "/search"
         },
     ]
   return (
     <>
-    <ul className='flex items-center gap-8 max-[990px]:hidden max-[1100px]:gap-5'>
         {navLinks?.map(item => (
-                <li className={`${Mode ? "text-black" : "text-white-person"}`} key={item.id}>
+            <ul key={item.id}>
+                <li className={`${Mode ? "text-black" : "text-white-person"}`}>
                     <NavLink to={item.link} className='link'>
                         <div className='flex flex-col items-center gap-2'>
                             {item.icon}
@@ -48,10 +46,11 @@ const Navlinks = () => {
                         </div>
                     </NavLink>
                 </li>
+            </ul>
+
         ))}
-    </ul>
     </>
   )
 }
 
-export default React.memo(Navlinks)
+export default Navlinks
