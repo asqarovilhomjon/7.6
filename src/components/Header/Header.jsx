8f7837ad9e9df8a1a397b3c9170cbdb5178cb1b7
asqarovilhomjon <asqarovilhomjon045@gmail.com>
2024-12-09 12:59:30 +0500

    cinema

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -45,7 +45,6 @@ const Header = () => {
 
   return (
     <>
-      {/* Header */}
       <header
         className={`${
           Mode ? "bg-[#fff]" : "bg-[#000]"
@@ -71,6 +70,7 @@ const Header = () => {
           </nav>
         </div>
       </header>
+  
       <div
         onClick={handleCloseModals}
         className={`w-screen h-screen fixed text-center top-[89px] z-[90] ${
@@ -80,123 +80,24 @@ const Header = () => {
               menu ? "opacity-100 translate-y-0" : "opacity-0 translate-y-full"
             }`}
       >
-        <div className={`${Mode ? "bg-white" : " bg-black"} pt-5`}>
-          <ul className="flex flex-col mb-7">
-            <li
-              className={`py-[15px] ${
-                Mode ? "text-black" : "text-white-person"
-              }`}
-            >
-              <NavLink
-                onClick={handleMenuToggle}
-                to={"/"}
-                className="flex justify-center"
-              >
-                <p className="flex gap-1 items-center">
-                  <BiMovie className="w-6 h-6 " /> <span>Афиша</span>
-                </p>
-              </NavLink>
-            </li>
-            <li
-              className={`py-[15px] ${
-                Mode ? "text-black" : "text-white-person"
-              }`}
-            >
-              <NavLink
-                onClick={handleMenuToggle}
-                to={"/sessions"}
-                className="flex justify-center"
-              >
-                <p className="flex gap-1 items-center">
-                  <MdOutlineTheaterComedy className="w-6 h-6 " />{" "}
-                  <span>Сеансы</span>
-                </p>
-              </NavLink>
-            </li>
-            <li
-              className={`py-[15px] ${
-                Mode ? "text-black" : "text-white-person"
-              }`}
-            >
-              <NavLink
-                onClick={handleMenuToggle}
-                to={"/ticket"}
-                className="flex justify-center"
-              >
-                <p className="flex gap-1 items-center">
-                  <TbTicket className="w-6 h-6 " /> <span>Билеты</span>
-                </p>
-              </NavLink>
-            </li>
-            <li
-              className={`py-[15px] ${
-                Mode ? "text-black" : "text-white-person"
-              }`}
-            >
-              <NavLink
-                onClick={handleMenuToggle}
-                to={"/search"}
-                className="flex justify-center"
-              >
-                <p className="flex gap-1 items-center">
-                  <FiSearch className="w-6 h-6 " /> <span>Поиск</span>
-                </p>
-              </NavLink>
-            </li>
-          </ul>
-          <div className="pb-8 mt-8">
-            <button className="bg-red-person text-white-person px-7 py-2 rounded-lg ">
-              Войти
-            </button>
-          </div>
-          <div className="flex justify-center pb-10 h-14 z-50 mx-auto relative">
-          <button
-                onClick={handleLanguageToggle}
-                className="flex items-center gap-1 text-white hover:text-gray-300 transition-colors"
-              >
-                <img src={ru} alt="Language Flag" className="w-5 h-5 rounded-full" />
-                <span
-                  className={`${
-                    Mode ? "text-black" : "text-white-person"
-                  } font-aeonik`}
-                >
-                  {selectedLanguage}
-                </span>
-                <MdKeyboardArrowDown
-                  className={`w-5 h-5 transition-transform ${
-                    isOpen ? "rotate-180" : ""
-                  } ${Mode ? "text-black" : "text-white-person"}`}
-                />
-              </button>
-              {isOpen && (
-                <div
-                  className={`absolute top-full ${
-                    Mode ? "bg-black text-white" : "bg-white text-black"
-                  } mt-2 border border-gray-700 rounded-md py-1 min-w-[100px]`}
-                >
-                  {languages.map((lang, inx) => (
-                    <button
-                      key={inx}
-                      className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                      onClick={() => handleLanguageChange(lang)}
-                    >
-                      {lang.label}
-                    </button>
-                  ))}
-                </div>
-              )}
-          </div>
-        </div>
       </div>
-      {header ? (
+  
+      {header && (
         <div className="fixed bottom-16 right-[11%] z-[101] duration-300 max-[990px]:right-[5%] max-[990px]:bottom-9 max-[550px]:bottom-6">
           <a href="#">
             <IoArrowUpCircle className="text-[50px] text-red-person" />
           </a>
         </div>
-      ) : null}
+      )}
+  
+      {Mode && (
+        <div className="fixed bottom-0 left-0 w-full bg-gray-800 text-white py-2 text-center">
+          Qorong‘i rejim yoqilgan
+        </div>
+      )}
     </>
   );
+  
 };
 
 export default React.memo(Header);
